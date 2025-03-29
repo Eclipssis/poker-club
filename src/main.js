3565ee7bf5b4469f47f563c6b692b1721bcdef95
eclipssis <eclipssis.star@gmail.com>
2025-03-29 14:00:05 +0200

    add prime vue ui libriary

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+app.use(router);
+app.mount("#app");
