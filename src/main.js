606a5fcdc07f67372d1a987838a674393462b6bc
eclipssis <eclipssis.star@gmail.com>
2025-03-29 15:06:34 +0200

    add firebase

--- src/main.js
+++ src/main.js
@@ -6,6 +6,26 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
+// Firebase
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyEZyE854Upr-uU3UAlhQVXE4pr-zdCkA",
+  authDomain: "poker-club-7cd8a.firebaseapp.com",
+  projectId: "poker-club-7cd8a",
+  storageBucket: "poker-club-7cd8a.firebasestorage.app",
+  messagingSenderId: "856681032803",
+  appId: "1:856681032803:web:60464427a40d93fab99c56",
+  measurementId: "G-GW6YRDZWRK",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+console.log("firebaseApp", firebaseApp);
+const analytics = getAnalytics(firebaseApp);
+console.log("analytics", analytics);
+
 const app = createApp(App);
 app.use(PrimeVue, {
   theme: {
